74aca7f717dec400246ffd3ab9b7568296440fb2
iwa <iwaongit@gmail.com>
2021-12-13 16:50:44 +0100

    chore: :speech_balloon: update some phrasing

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -5,7 +5,7 @@ import Command from '../structures/Command';
 export default new class SetupCommand extends Command {
 
     public constructor() {
-        super('setup', SetupVC, 1, [], ['EMBED_LINKS'], 'setup', "Setup the bot in this VC");
+        super('setup', SetupVC, 1, [], ['EMBED_LINKS'], 'setup', "Setup the TTS Text channel for the bot");
     }
 
 }
@@ -14,7 +14,7 @@ async function SetupVC(interaction: CommandInteraction) {
     Bot.currentVC = interaction.channelId;
 
     await interaction.reply({
-        embeds: [Bot.createEmbed(null, `:white_check_mark: TTS Channel set to this channel!`)],
+        embeds: [Bot.createEmbed(null, `:white_check_mark: TTS Channel set to this text channel!`)],
         ephemeral: true
     })
         .then(() => { Bot.log.info({ msg: 'setup', author: { id: interaction.user.id, name: interaction.user.tag }, guild: { id: interaction.guild.id, name: interaction.guild.name } }); })
